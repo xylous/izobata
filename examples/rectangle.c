d5a2905c53bfaace875347e6283c398b4720503a
xylous <xylous.e@gmail.com>
2022-06-11 15:53:55 +0300

    Add an example where a rectangle is drawn

---
+++ examples/rectangle.c
@@ -0,0 +1,32 @@
+/* Copyright (c) 2022 xylous <xylous.e@gmail.com>
+ *
+ * This file is part of izobata.
+ * izobata is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * izobata is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * izobata.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "../src/izobata.h"
+
+int main(void)
+{
+    izobata_init();
+
+    Point *tl = new_point(10, 10);
+
+    Polygon *rect = rectangle(tl, 10, 5);
+    draw_polygon(rect);
+
+    output_all();
+    izobata_close();
+
+    return 0;
+}
